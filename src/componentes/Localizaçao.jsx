e77ee542989c7cdff0cf94c57c0bf7118f02fc34
gabriel lima <gabriellima140806@gmail.com>
2024-08-25 17:51:25 -0300

    site pronto

---
+++ src/componentes/Localizaçao.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../app.css'
+export default function Localizaçao() {
+  return (
+   /* From Uiverse.io by JaydipPrajapati1910 */ 
+<a href="https://www.google.com/maps/dir//Av.+Felipe+Carrillo+Puerto,+998+-+Jardim+Iae,+São+Paulo+-+SP,+05890-000/@-23.6671835,-46.8684827,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x94ce536849478285:0x7a5e442e58cea1f7!2m2!1d-46.7862537!2d-23.6673624?entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D">< div  class="loader"></div></a>
+  )
+}
